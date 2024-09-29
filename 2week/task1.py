edd96e7d32aa7dbf14175e638bf8d3a47b350f14
dan3t <dipiaterikov@edu.hse.ru>
2024-09-29 13:40:57 +0300

    added task1.py 2week

---
+++ 2week/task1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        map = {}
+        max_len = 0
+        left = 0
+        for right in range(0, len(s)):
+            if s[right] not in map or map[s[right]] < left:
+                map[s[right]] = right
+                max_len = max(max_len, right - left + 1)
+            else:
+                left = map[s[right]] + 1
+                map[s[right]] = right
+        return max_len
