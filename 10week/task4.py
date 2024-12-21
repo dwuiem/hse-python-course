b10a2362a2495c67fc6ed9fc06d3c84dc952afad
dwuiem <denisladkiy@gmail.com>
2024-12-21 19:30:25 +0300

    9 and 10 weeks

---
+++ 10week/task4.py
@@ -0,0 +1,36 @@
+"""
+URL: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+"""
+from collections import deque
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        if not root:
+            return res
+        queue = deque([root])
+        left_to_right = True
+        while queue:
+            level = deque()
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if left_to_right:
+                    level.append(node.val)
+                else:
+                    level.appendleft(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(list(level))
+            left_to_right = not left_to_right
+        return res
